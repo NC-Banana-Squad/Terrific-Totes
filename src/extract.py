5594ab0eadfbdcbaab01f490fdc0b451173d31e1
yellowchimney <irina.redactor@gmail.com>
2024-11-12 12:36:07 +0000

    created logic for initial extract, started logic for continuous extract

--- src/extract.py
+++ src/extract.py
@@ -6,15 +6,12 @@ import logging
 import boto3
 from pprint import pprint
 import csv
-#from botocore import botocore
+import io
 
-
-s3_client = boto3.client('s3')
 
 def connect():
     dotenv.load_dotenv()
 
-    # cohort_id = os.environ['cohort_id']
     user = os.environ['user']
     database = os.environ['database']
     password = os.environ['password']
@@ -30,38 +27,87 @@ def connect():
         port=port
     )
 
-def fetch():
-    conn = connect()
-    rows = conn.run('SELECT * FROM sales_order')
-    columns = [col['name'] for col in conn.columns]
+
+# def fetch():
+#     conn = connect()
+#     rows = conn.run('SELECT * FROM sales_order')
+#     columns = [col['name'] for col in conn.columns]
 
 
-    with open('data_fetch.csv', 'w', encoding='utf-8') as csv_f:
-        writer = csv.writer(csv_f)
-        writer.writerow(columns)
-        writer.writerows(rows)
+#     with open('data_fetch.csv', 'w', encoding='utf-8') as csv_f:
+#         writer = csv.writer(csv_f)
+#         writer.writerow(columns)
+#         writer.writerows(rows)
     
 
-fetch()
+# fetch()
 
 
 
 def lambda_handler(event, context):
     s3_client = boto3.client('s3')
-    conn = connect()
-    rows = conn.run('SELECT * FROM sales_order')
-    columns = [col['name'] for col in conn.columns]
+    if 'last_extracted.txt' in s3_client.list_objects(Bucket='banana-squad-code'):
+        continuous_extract()
+    else:
+        initial_extract()
 
-    #table_name = [name for name in tables]
+    last_extracted = datetime.now().isoformat().replace('T',' ')
+    s3_client.put_object(Body=last_extracted, 
+                                 Bucket='banana-squad-code', 
+                                 Key='last_extracted.txt')
+    
+
+
+def initial_extract():    
+    s3_client = boto3.client('s3')
+    conn = connect()
+    query = conn.run('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' AND table_name != \'_prisma_migrations\'')
+    # all_tables = conn.run('SELECT tables FROM information_')
+
+    table_name = [name for name in query]
     year = datetime.now().strftime('%Y')
     month = datetime.now().strftime('%m')
     day = datetime.now().strftime('%d')
     time_now = datetime.now().isoformat()
-    file_name = f'{year}/{month}/{day}/{time_now}.csv'
-    try:
-        json_data = json.dumps(event)
-        s3_client.put_object(Body=json_data, Bucket=data_bucket_name, Key=file_name)
-        return {"result": "Success"}
-    except Exception:
-        return {"result": "Failure"}
+    file_name = f'{table_name}/{year}/{month}/{day}/{time_now}.csv'
 
+    for table in query:
+        rows = conn.run(f'SELECT * FROM {table}')
+        columns = [col['name'] for col in conn.columns]
+
+        csv_buffer= io.StringIO()
+        writer = csv.writer(csv_buffer)
+        writer.writerow(columns)
+        writer.writerows(rows)
+
+        csv_buffer.seek(0)
+
+        try:
+            s3_client.put_object(Body=csv_buffer.getvalue(), 
+                                 Bucket='banana-squad-code', 
+                                 Key=file_name)
+            return {"result": "Success"}
+        except Exception:
+            return {"result": "Failure"}
+
+    conn.close()
+   
+def continuous_extract():
+    s3_client = boto3.client('s3')
+    conn = connect()
+    response = s3_client.get_object(Bucket='banana-squad-code', Key='last_extracted.txt')
+    contents = response['Body'].read()
+    readable_content = contents.decode('utf-8')
+    query = conn.run('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' AND table_name != \'_prisma_migrations\'')
+    table_name = [name for name in query]
+
+    for table in table_name:
+        query = conn.run('SELECT created_at FROM sales_order WHERE created_at >')
+
+# time = datetime.now().isoformat().replace('T',' ')
+# print(time)
+
+# s3_client = boto3.client('s3')
+# conn = connect()
+# query = conn.run('SELECT created_at, last_updated FROM sales_order WHERE created_at != last_updated')
+# print(query)
