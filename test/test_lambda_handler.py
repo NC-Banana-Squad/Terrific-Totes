0fc958dfc562815c3eaea98661dbfd8642eb7e6f
Datasapien <bradleyclayton89@gmail.com>
2024-11-14 11:58:14 +0000

    Create file for testing lambda_handler

---
+++ test/test_lambda_handler.py
@@ -0,0 +1,9 @@
+from src.extract import lambda_handler
+from moto import mock_aws
+from unittest.mock import patch
+from botocore.exceptions import NoCredentialsError
+import pytest
+import boto3
+
+def test():
+    pass
