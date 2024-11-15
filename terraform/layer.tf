b8e7147d91fb9bafc847f36dfb977ab26c829ad6
yellowchimney <irina.redactor@gmail.com>
2024-11-15 23:30:09 +0000

    test

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -1,60 +1,29 @@
+#Need to use --upgrade when targeting a folder so that the install overwrites functions.
 resource "null_resource" "create_dependencies" {
+    # Install Python dependencies listed in a extract_layer_requirements.txt
   provisioner "local-exec" {
-    command = <<EOT
-      rm -rf ${path.module}/../extract_layer/python/*
-      pip install -r ${path.module}/../extract_layer_requirements.txt --upgrade -t ${path.module}/../extract_layer/python
-    EOT
-    environment = {
-      GITHUB_WORKSPACE = "${path.module}"  # Set the environment variable for the current workspace
-    }
+    command = "rm -rf ${path.module}/../extract_layer/python/* && pip install -r ${path.module}/../extract_layer_requirements.txt --upgrade -t ${path.module}/../extract_layer/python"
   }
   triggers = {
     dependencies = filemd5("${path.module}/../extract_layer_requirements.txt")
   }
 }
 
+
+  #Creates zip file after create_dependencies
 resource "null_resource" "create_extract_layer_archive" {
   depends_on = [null_resource.create_dependencies]
 
   provisioner "local-exec" {
     command = <<EOT
-      layer_dir="${GITHUB_WORKSPACE}/extract_layer"
-      mkdir -p $layer_dir  # Ensure directory exists
-      # Ensure the install worked by checking files are in the directory
-      if [ "$(ls -A $layer_dir)" ]; then
-        cd $layer_dir && zip -r ${GITHUB_WORKSPACE}/extract_layer.zip ./*
-      else
-        echo "No files to zip, skipping..."
-      fi
+      cd ${path.module}/../extract_layer && zip -r ${path.module}/../extract_layer.zip ./*
     EOT
-    environment = {
-      GITHUB_WORKSPACE = "${path.module}"  # Pass the current module path as environment variable
-    }
   }
+  # To make sure that extract layer archive is recreated(even when it's empty) on each run
   triggers = {
-    archive_created = timestamp()
+    archive_created = timestamp() 
   }
 }
-
-resource "aws_lambda_layer_version" "dependency_layer" {
-  layer_name          = "dependency_layer"
-  compatible_runtimes = [var.python_runtime]
-  s3_bucket           = aws_s3_bucket.code_bucket.bucket
-  s3_key              = aws_s3_object.extract_layer_code.key
-  depends_on = [null_resource.create_dependencies]
-}
-
-
-
-  # Deletes zip file and extract layer folder content when terraform destroy
-resource "null_resource" "cleanup" {
-  provisioner "local-exec" {
-    when    = destroy
-    command = "rm -rf ${path.module}/../extract_layer.zip ${path.module}/../extract_layer/python"
-  }
-}
-
-
 
 # data "null_data_source" "wait_for_lambda_exporter" {
 #   inputs = {
@@ -81,3 +50,25 @@ data "archive_file" "extract_layer" {
   output_path      = "${path.module}/../extract_layer.zip"
   depends_on = [null_resource.archive_trigger]
 } */
+
+
+# Define
+resource "aws_lambda_layer_version" "dependency_layer" {
+  layer_name          = "dependency_layer"
+  compatible_runtimes = [var.python_runtime]
+  s3_bucket           = aws_s3_bucket.code_bucket.bucket
+  s3_key              = aws_s3_object.extract_layer_code.key
+  depends_on = [null_resource.create_dependencies]
+  #source_code_hash = filebase64sha256("${path.module}/../extract_layer.zip")
+}
+
+
+  # Deletes zip file and extract layer folder content when terraform destroy
+resource "null_resource" "cleanup" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf ${path.module}/../extract_layer.zip ${path.module}/../extract_layer/python"
+  }
+}
+
+
