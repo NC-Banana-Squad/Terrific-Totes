02ccaeef60b9e2916122246636cb7cbb423fabcf
CiaranKyle <ciarankyle@gmail.com>
2024-11-21 15:22:02 +0000

    Changes made to the layers through deploy file and terraform files

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_layer_version" "dependency_layer" {
   layer_name          = "dependency_layer"
   compatible_runtimes = [var.python_runtime]
   s3_bucket           = aws_s3_bucket.code_bucket.bucket
-  s3_key              = aws_s3_object.extract_layer.key
+  s3_key              = aws_s3_object.extract_layer_code.key
 }
 
 output "layer_arn_with_version" {
