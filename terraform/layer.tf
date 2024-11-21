154469e6f6e4031650447f04df24cb0734bf2d7e
yellowchimney <irina.redactor@gmail.com>
2024-11-21 14:56:04 +0000

    added separate CW metric alarm in cloudwatch.tf

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -3,5 +3,5 @@ resource "aws_lambda_layer_version" "dependency_layer" {
   layer_name          = "dependency_layer"
   compatible_runtimes = [var.python_runtime]
   s3_bucket           = aws_s3_bucket.code_bucket.bucket
-  s3_key              = aws_s3_object.extract_layer_code.key
+  s3_key              = aws_s3_object.extract_layer.key
 }
