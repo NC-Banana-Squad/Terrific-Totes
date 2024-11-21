879095abd4472783e6416f148e096ddaf9a77be7
yellowchimney <irina.redactor@gmail.com>
2024-11-21 15:09:54 +0000

    added versioning for the layer to ensure it updates

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -5,3 +5,7 @@ resource "aws_lambda_layer_version" "dependency_layer" {
   s3_bucket           = aws_s3_bucket.code_bucket.bucket
   s3_key              = aws_s3_object.extract_layer.key
 }
+
+output "layer_arn_with_version" {
+  value = aws_lambda_layer_version.dependency_layer.arn
+}
