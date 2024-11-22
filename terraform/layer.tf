8de994c4c195030d7bd7d054fca959aa5aa58b43
CiaranKyle <ciarankyle@gmail.com>
2024-11-22 11:41:59 +0000

    Changes to requirements and layer.tf

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -4,4 +4,5 @@ resource "aws_lambda_layer_version" "dependency_layer" {
   compatible_runtimes = [var.python_runtime]
   s3_bucket           = aws_s3_bucket.code_bucket.bucket
   s3_key              = aws_s3_object.extract_layer_code.key
+  source_code_hash = filebase64sha256("${path.module}/../extract_layer.zip")
 }
