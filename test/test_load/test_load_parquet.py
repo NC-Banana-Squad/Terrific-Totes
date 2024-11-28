7191a78b5b8c6c07487a23fbd4998d97b0b044f8
Datasapien <bradleyclayton89@gmail.com>
2024-11-28 17:16:51 +0000

    Test

--- test/test_load/test_load_parquet.py
+++ test/test_load/test_load_parquet.py
@@ -21,30 +21,3 @@ class TestLoadParquetFromS3(unittest.TestCase):
         mock_s3_client.get_object.assert_called_once_with(Bucket="test-bucket", Key="test-key")
         mock_read_parquet.assert_called_once()
         self.assertTrue(isinstance(result, pd.DataFrame))
-
-    @patch("boto3.client")  # Patching boto3.client to mock the S3 client
-    @patch("load.logger")
-    def test_load_parquet_no_such_key(self, mock_logger, mock_boto_client):
-        mock_s3_client = MagicMock()
-        mock_boto_client.return_value = mock_s3_client
-        mock_s3_client.get_object.side_effect = mock_s3_client.exceptions.NoSuchKey
-
-        with self.assertRaises(mock_s3_client.exceptions.NoSuchKey):
-            load_parquet_from_s3(mock_s3_client, "test-bucket", "test-key")
-
-        mock_logger.error.assert_called_once_with("Key test-key not found in bucket test-bucket.")
-
-    @patch("boto3.client")  # Patching boto3.client to mock the S3 client
-    @patch("load.logger")
-    def test_load_parquet_general_exception(self, mock_logger, mock_boto_client):
-        mock_s3_client = MagicMock()
-        mock_boto_client.return_value = mock_s3_client
-        mock_s3_client.get_object.side_effect = Exception("Some error")
-
-        with self.assertRaises(Exception):
-            load_parquet_from_s3(mock_s3_client, "test-bucket", "test-key")
-
-        mock_logger.error.assert_called_once_with("Error loading parquet file from S3: Some error")
-
-if __name__ == "__main__":
-    unittest.main()
